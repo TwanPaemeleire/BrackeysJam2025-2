fc66cc87df14dc495815777f5ee79b6f0fd71361
Samir-V <samirs.vertahovs@gmail.com>
2025-08-26 17:05:55 +0200

    Added basic sound manager

---
+++ Assets/Scripts/General/SoundManager.cs
@@ -0,0 +1,60 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.General
+{
+    public class SoundManager : MonoSingleton<SoundManager>
+    {
+        [System.Serializable]
+        public struct SFXEntry
+        {
+            public string key;
+            public AudioClip clip;
+        }
+
+        [SerializeField]
+        private AudioSource _sfxSource;
+
+        [SerializeField]
+        private List<SFXEntry> _sfxEntries = new List<SFXEntry>();
+        private Dictionary<string, AudioClip> _sfxLibrary = new Dictionary<string, AudioClip>();
+
+        private void Awake()
+        {
+            if (_sfxSource == null)
+            {
+                _sfxSource = gameObject.AddComponent<AudioSource>();
+                _sfxSource.loop = false;
+                _sfxSource.playOnAwake = false;
+            }
+        }
+
+        public void PlaySFX(AudioClip clip, float volume = 1f)
+        {
+            if (clip == null)
+            {
+                return;
+            }
+            _sfxSource.PlayOneShot(clip, volume);
+        }
+
+        public void RegisterSFX(string key, AudioClip clip)
+        {
+            if (!_sfxLibrary.ContainsKey(key) && clip != null)
+            {
+                _sfxLibrary.Add(key, clip);
+            }
+        }
+
+        public void PlaySFX(string key, float volume = 1f)
+        {
+            var entry = _sfxEntries.FirstOrDefault(e => e.key == key);
+
+            if (entry.clip != null && entry.key == key)
+            {
+                _sfxSource.PlayOneShot(entry.clip, volume);
+            }
+        }
+    }
+}
