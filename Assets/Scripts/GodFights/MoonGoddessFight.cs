84f7ea87595bc6a039383a2205972fe0867576f2
Samir-V <samirs.vertahovs@gmail.com>
2025-08-30 15:44:14 +0200

    Finalized Moon Boss movement

--- Assets/Scripts/GodFights/MoonGoddessFight.cs
+++ Assets/Scripts/GodFights/MoonGoddessFight.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 namespace Assets.Scripts.GodFights
@@ -7,6 +8,17 @@ namespace Assets.Scripts.GodFights
         [SerializeField]
         private GameObject _floatingAvatarObject;
 
+        [SerializeField] 
+        private Transform _centerPoint;
+        [SerializeField] 
+        private float _radiusX = 6.0f;      
+        [SerializeField] 
+        private float _radiusY = 2.0f;      
+        [SerializeField] 
+        private float _speed = 0.5f;
+
+        private float _angle;
+
         public override void StartBossFight()
         {
             base.StartBossFight();
@@ -14,6 +26,9 @@ namespace Assets.Scripts.GodFights
             _floatingAvatarObject.SetActive(true);
 
             Animator.SetTrigger("Spawn");
+
+            StartCoroutine(nameof(StartEllipseMovement));
+            StartCoroutine(nameof(StartShooting));
         }
 
         public override void RestartBossFight()
@@ -23,12 +38,59 @@ namespace Assets.Scripts.GodFights
 
         protected override void OnDeathInternal()
         {
+            _floatingAvatarObject.SetActive(false);
             OnDeath.Invoke();
         }
 
         public void OnSpawnFinished()
         {
             Animator.SetTrigger("Move");    
+        }
+
+        private IEnumerator StartEllipseMovement()
+        {
+            while (true)
+            {
+                _angle += Time.deltaTime * _speed;
+
+                var x = Mathf.Cos(_angle) * _radiusX;
+                var y = Mathf.Sin(_angle) * _radiusY;
+
+                var pos = _floatingAvatarObject.transform.position;
+                var newPos = _centerPoint.position + new Vector3(x, y, 0);
+
+                var dir = newPos - pos;
+
+                if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+                {
+                    if (dir.x > 0)
+                    {
+                        transform.localScale = new Vector3(1, 1, 1);
+                    }
+                    else if (dir.x < 0)
+                    {
+                        transform.localScale = new Vector3(-1, 1, 1);
+                    }
+                }
+
+                transform.position = newPos;
+
+                yield return null;
+            }
+        }
+
+        private IEnumerator StartShooting()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(Random.Range(2f, 5f));
+                ShootProjectile();
+            }
+        }
+
+        private void ShootProjectile()
+        {
+
         }
     }
 }
