e626ad2f8fd42987e8db49f95728eba21768c615
TwanPaemeleire <161451506+TwanPaemeleire@users.noreply.github.com>
2025-08-30 18:58:32 +0200

    Added and implemented button UI sprites.

--- Assets/Scripts/UI/CreditSceneController.cs
+++ Assets/Scripts/UI/CreditSceneController.cs
@@ -3,8 +3,11 @@ using UnityEngine;
 
 public class CreditSceneController : MonoBehaviour
 {
+    private bool _isReturning = false;
     public void ReturnToMainMenu()
     {
+        if (_isReturning) return;
+        _isReturning = true;
         Destroy(FightSequenceManager.Instance.gameObject);
         Destroy(DialogueManager.Instance.gameObject);
     }
