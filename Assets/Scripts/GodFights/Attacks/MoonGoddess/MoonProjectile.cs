027d8ab3d239c37433e06228b0d23f5a4767291a
TwanPaemeleire <161451506+TwanPaemeleire@users.noreply.github.com>
2025-08-31 04:13:07 +0200

    Added and implemented a new sound, and fixed a bug with the moon goddess projectile.

--- Assets/Scripts/GodFights/Attacks/MoonGoddess/MoonProjectile.cs
+++ Assets/Scripts/GodFights/Attacks/MoonGoddess/MoonProjectile.cs
@@ -13,10 +13,13 @@ namespace Assets.Scripts.GodFights.Attacks.MoonGoddess
         [SerializeField]
         private Animator _animator;
 
-        [SerializeField]
-        private AudioClip _shootAudioClip;
+        [SerializeField] private AudioClip _shootAudioClip;
+        [SerializeField] private AudioClip _groundHitAudioClip;
+        [SerializeField] private float _shootAudioClipVolume = 0.05f;
+        [SerializeField] private float _groundHitAudioClipVolume = 0.05f;
 
-        private Vector3 _directionToPlayer;
+        private Rigidbody2D _rigidBody;
+        private Vector2 _directionToPlayer;
 
         private void Awake()
         {
@@ -25,6 +28,8 @@ namespace Assets.Scripts.GodFights.Attacks.MoonGoddess
 
         private void Start()
         {
+            _rigidBody = GetComponent<Rigidbody2D>();
+
             var sword = FightSequenceManager.Instance.PlayerObject.GetComponent<PlayerSword>();
 
             _directionToPlayer = (sword.PlayerCenterPoint.position - transform.position).normalized;
@@ -32,12 +37,13 @@ namespace Assets.Scripts.GodFights.Attacks.MoonGoddess
             var angle = Mathf.Atan2(_directionToPlayer.y, _directionToPlayer.x) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0, 0, angle + 90.0f);
 
-            SoundManager.Instance.PlaySFX(_shootAudioClip, 0.3f);
+            SoundManager.Instance.PlaySFX(_shootAudioClip, _shootAudioClipVolume);
         }
 
         private void Update()
         {
-            transform.position += _projectileSpeed * Time.deltaTime * _directionToPlayer;
+            Vector2 newPosition = _rigidBody.position + _directionToPlayer * _projectileSpeed * Time.fixedDeltaTime;
+            _rigidBody.MovePosition(newPosition);
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
@@ -52,12 +58,14 @@ namespace Assets.Scripts.GodFights.Attacks.MoonGoddess
                 {
                     sword.OnSuccesfullParryExecuted();
                 }
-
+                GetComponent<BoxCollider2D>().enabled = false;
                 _animator.SetTrigger("Hit");
             }
 
             if (collision.CompareTag("Ground"))
             {
+                GetComponent<BoxCollider2D>().enabled = false;
+                SoundManager.Instance.PlaySFX(_groundHitAudioClip, _groundHitAudioClipVolume);
                 _animator.SetTrigger("Hit");
                 _projectileSpeed = 0;
             }
